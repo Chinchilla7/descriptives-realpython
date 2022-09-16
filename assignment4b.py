9d37d690eed1cb683f11a16725b90224741c3d48
Chinchilla7 <regina.chen@stonybrook.edu>
2022-09-16 15:30:52 -0400

    Update assignment4b.py

--- assignment4b.py
+++ assignment4b.py
@@ -1,1 +1,17 @@
+import researchpy as rp
+import pandas as pd
+
+#load in data
+df = pd.read_csv('data.csv')
+df.columns
+
+#Prints out descriptive information for DataFrame.
+rp.codebook(df)
+
+#getting descriptive of single/continuous variables
+rp.summary_cont(df[['Age', 'HR', 'sBP']])
+
+#getting descriptive of categorical variables
+rp.summary_cat(df[['Group', 'Smoke']])
+
 
